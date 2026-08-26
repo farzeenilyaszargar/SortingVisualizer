f980b27a04ebd5126daa407d4ccfc1de1c14e211
farzeenilyaszargar <farzeenilyaszargar@gmail.com>
2026-08-27 01:07:51 +0530

    changes

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # SortingVisualizer
+
+Just visualizing sorting algorithms basic ahh shi
+
+  
+
+\- bubble sort
+
+\- selection sort
+
+\- insertion sort
+
+\- merge sort
+
+\- quick sort
+
+\- heap sort
---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int bubbleSort(int arr[], int n)
+{
+    int sorted = {};
+
+    for (int i = 0; i<n; i++)
+    {
+
+    }
+
+    return sorted;
+
+}
+
+
+int main()
+{
+    int algo = 0;
+
+    int arr[] = {1,2,3,5, 6, 2, 1, 5, 3, 212, 1};
+
+    
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+
+
+
+    return 0;
+
+}
